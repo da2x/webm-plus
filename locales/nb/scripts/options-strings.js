06363f04941445d5a38c37cef5f4d47460df41af
Daniel Aleksandersen <github@daniel.priv.no>
2011-10-27 01:22:18 +0200

    New Norwegian bokmål translation.

---
+++ locales/nb/scripts/options-strings.js
@@ -0,0 +1,10 @@
+var i18n = {
+  '%widgetname options': widget.name + ' valg',
+  'continueTesting': 'Forbli innmeldt i YouTube HTML5-testen',
+  'videoSaveButton': 'Legg til en ‹Last ned video› knapp under alle WebM videoer',
+  'filterSearch': 'Filtrer søkeresultater til kun WebM videoer',
+  'hideFlashPromo': 'Skjul reklame for Adobe Flash',
+  'hideFlashUpgrade': 'Skjul oppgraderingsnotiser for Adobe Flash',
+  'preventFlash': 'Hindre all videoavspilling ved hjelp av  Adobe Flash tillegget',
+  'Done': 'Ferdig'
+};
