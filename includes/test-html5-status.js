eb71b98488de98e1b9000b4980a58973b67f5396
Daniel Aleksandersen <github@daniel.priv.no>
2011-10-30 01:58:23 +0200

    Added more safe checks, renamed the file to reflect new usage, and updated internal messages for communicating with background process.

--- includes/test-html5-status.js
+++ includes/test-html5-status.js
@@ -6,44 +6,45 @@
 // ==/UserScript==
 
 (function() {
+  var form = document.getElementById('html5form');
   window.addEventListener('DOMContentLoaded', function()
   {
     submitFormWithSessionToken();
   }, false);
 
-  var form = document.getElementById('html5form');
-
   function submitFormWithSessionToken()
   {
     if (widget.preferences.continueTesting == 'true')
     {
-      if (form)
+      if (form != null)
       {
         if (form.querySelector("input[name='enable_html5']"))
         {
           if (!form.querySelector("input[name='session_token']"))
           {
-           form.addEventListener('DOMNodeInserted', formSubmitter, false);
+            form.addEventListener('DOMNodeInserted', submitForm, false);
           }
-          else formSubmitter();
+          else submitForm();
         }
         else if (form.querySelector("input[name='disable_html5']"))
         {
           continueTesting();
-          opera.extension.postMessage('testCandidate');
+          opera.extension.postMessage('goodHtml5VideoStatus');
       }}
-      else opera.extension.postMessage('discontinuedTesting');
+      else opera.extension.postMessage('badHtml5VideoStatus');
   }}
 
-  function formSubmitter()
+  function submitForm()
   {
     form.submit();
   }
 
   function continueTesting()
-  { // keep the test subjects in the HTML5 Trial
-    var enrichmentCentre = document.getElementById('html5-join-link'),
-    escapeElevator = enrichmentCentre.getElementsByTagName('h3')[0];
-    enrichmentCentre.removeChild(escapeElevator);
-  } // ... there is no escape ...
+  {  // keep the test subjects in the HTML5 Trial
+    var enrichmentCentre = document.getElementById('html5-join-link');
+    if (enrichmentCentre != null)
+    {
+      var escapeElevator = enrichmentCentre.getElementsByTagName('h3')[0];
+      enrichmentCentre.removeChild(escapeElevator);
+  }} // ... there is no escape ...
 }());
