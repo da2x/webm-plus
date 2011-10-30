7abf3da9cab32ff5e4c0fda950eaa63ebd4e1f51
Daniel Aleksandersen <github@daniel.priv.no>
2011-10-30 16:15:02 +0100

    Added empty strings for new neverReload option to all locales.

--- locales/uk/scripts/options-strings.js
+++ locales/uk/scripts/options-strings.js
@@ -3,6 +3,7 @@ var i18n = {
   'Rejoin the YouTube HTML5 Trial automatically': 'Автоматично приєднатися до тестової програми YouTube HTML5',
   'Add a ‘Download Video’ button below all WebM videos': 'Додати кнопку "Завантажити відео" до всіх WebM роликів',
   'Filter search result to WebM videos only': 'Обмежити результати пошуку роликами у форматі WebM',
+  'Never reload any embedded- or video pages': '',
   'Hide promotions for Adobe Flash': 'Сховати пропозиції щодо завантаження плагіну Adobe Flash',
   'Hide upgrade warnings for Adobe Flash': 'Не нагадувати про оновлення Adobe Flash',
   'Prevent all video playback using the Adobe Flash plug-in': 'Не програвати навіть ті ролики, що доступні лише у форматі Adobe Flash',
