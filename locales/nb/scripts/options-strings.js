7635b4297df77cccaa3b80554ed5bf1efbc6eca5
Daniel Aleksandersen <github@daniel.priv.no>
2011-11-06 01:52:55 +0100

    Removed localized strings for filterSearch and hideUpgrade options.

--- locales/nb/scripts/options-strings.js
+++ locales/nb/scripts/options-strings.js
@@ -2,10 +2,8 @@ var i18n = {
   '%widgetname options': widget.name + ' valg',
   'Rejoin the YouTube HTML5 Trial automatically': 'Forbli innmeldt i YouTube HTML5-testen',
   'Add a ‘Download Video’ button below all WebM videos': 'Legg til en ‹Last ned video› knapp under alle WebM videoer',
-  'Filter search result to WebM videos only': 'Filtrer søkeresultater til kun WebM videoer',
   'Never reload any embedded- or video pages': 'Aldri oppdater integrete- og videosider',
   'Hide promotions for Adobe Flash': 'Skjul reklame for Adobe Flash',
-  'Hide upgrade warnings for Adobe Flash': 'Skjul oppgraderingsnotiser for Adobe Flash',
   'Prevent all video playback using the Adobe Flash plug-in': 'Hindre all videoavspilling ved hjelp av  Adobe Flash tillegget',
   'Done': 'Ferdig'
 };
