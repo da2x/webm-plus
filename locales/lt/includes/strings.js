9edb7f1cce3c735c5810cb241b5e70834cec3af2
Daniel Aleksandersen <github@daniel.priv.no>
2011-11-07 23:23:38 +0100

    Merged pull request 7.

---
+++ locales/lt/includes/strings.js
@@ -0,0 +1,4 @@
+var oexWebMPlusi18n = {
+  'Download Video': 'Atsisiūsti video',
+  'Click, then press Ctrl+S to save.': 'Spragtelėkite, tuomet paspauskite Ctrl+S tam kad išsaugotumėte'
+};
---
+++ locales/lt/scripts/options-strings.js
@@ -0,0 +1,9 @@
+var i18n = {
+  '%widgetname options': 'Налаштування' + widget.name,
+  'Rejoin the YouTube HTML5 Trial automatically': 'Automatiškai pratęsti narystę YouTube HTML5 bandyme',
+  'Add a ‘Download Video’ button below all WebM videos': 'Rodyti "Parsisiūsti video" mygtuką po visais WebM video',
+  'Never reload any embedded- or video pages': 'Neperkrauti "embedded" ar video puslapių',
+  'Hide promotions for Adobe Flash': 'Nerodyti pasiūlymų skirtų Adobe Flash',
+  'Prevent all video playback using the Adobe Flash plug-in': 'Neleisti rodyti video naudojant Adobe Flash',
+  'Done': 'Baigta'
+};
