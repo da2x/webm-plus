ef77539eca49d855f61625300aefc09e1037c254
Daniel Aleksandersen <github@daniel.priv.no>
2011-12-06 17:54:25 +0100

    New Spanish translation via Transifex (by FernanDoylet at Coffee and Power)

---
+++ locales/es/includes/webm-plus-extension.i18n.js
@@ -0,0 +1,4 @@
+var oexi18n = {
+  "Click, then press Ctrl+S to save.":"Haga click, y despues presione Ctrl+S para grabar.",
+  "Download Video":"Bajar Video"
+}
---
+++ locales/es/scripts/options.i18n.js
@@ -0,0 +1,10 @@
+var oexi18n = {
+  "Add a ‘Download Video’ button below all WebM videos":"Agregar un boton 'Bajar Video' debajo de todos los videos WebM",
+  "Check HTML5 Trial participation status once per session":"Revisar el status de los participantes HTML5 de Prueba, una vez por sesion",
+  "Done":"Hecho",
+  "Embed alternate player instead of using Flash":"Insertar jugador alterno en lugar de usar Flash",
+  "Hide promotions for Adobe Flash Player":"Ocultar las promociones de Adobe Flash Player",
+  "Open source on Github":"Codigo abierto en Github",
+  "Request WebM for embedded videos on third-party Web sites":"Solicitar WebM para videos insertados en sitios Web de terceros",
+  "YouTube WebM Plus options":"Opciones de YouTube WebM Plus"
+}
