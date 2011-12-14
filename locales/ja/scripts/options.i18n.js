199e359497364b17bae391213c13487faec42ed5
Daniel Aleksandersen <github@daniel.priv.no>
2011-12-14 19:09:00 +0100

    Small update to Japanese translation.

--- locales/ja/scripts/options.i18n.js
+++ locales/ja/scripts/options.i18n.js
@@ -1,7 +1,7 @@
 var oexi18n = {
   "Add a ‘Download Video’ button below all WebM videos":"ずべてのWebM動画の下に「動画のダウンロード」のボタンを表示する。",
   "Check HTML5 Trial participation status once per session":"恪セッションごとに一回HTML5試験の参加状況を確認する。",
-  "Done":"終了",
+  "Done":"閉じる",
   "Embed alternate player instead of using Flash":"Flashを使用の変わりに代替プレーヤーをエンベッドする。",
   "Hide promotions for Adobe Flash Player":"Adobe Flash プレヤーのCMを隠す。",
   "Open source on Github":"Github上でオーペンソース",
