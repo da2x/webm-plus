95678204b901b784ee040f0236d232531ebd5de9
Daniel Aleksandersen <code@daniel.priv.no>
2012-02-05 16:23:35 +0100

    New Portuguese (Brazil) translation. Courtesy of jnsoares on Transifex.

---
+++ locales/pt-br/includes/webm-plus-extension.i18n.js
@@ -0,0 +1,4 @@
+var oexi18n = {
+  "Click, then press Ctrl+S to save.":"Clique e depois pressione Ctrl+S para salvar.",
+  "Download Video":"Baixe o Video"
+}
---
+++ locales/pt-br/scripts/options.i18n.js
@@ -0,0 +1,10 @@
+var oexi18n = {
+  "Add a ‘Download Video’ button below all WebM videos":"Adicione um botão &quot;Baixe o Video&quot; abaixo de todos os vídeos WebM",
+  "Check HTML5 Trial participation status once per session":"Verifique o status da participação no HTML5 Trial uma vez por sessão",
+  "Done":"Concluído",
+  "Embed alternate player instead of using Flash":"Embutir o player alternativo ao invés de usar o Flash",
+  "Hide promotions for Adobe Flash Player":"Ocultar propagandas do Adobe Flash Player",
+  "Open source on Github":"Código aberto no Github",
+  "Request WebM for embedded videos on third-party Web sites":"Solicitar WebM para videos embutidos em websites de terceiros",
+  "YouTube WebM Plus options":"Opções do YouTube WebM Plus"
+}
