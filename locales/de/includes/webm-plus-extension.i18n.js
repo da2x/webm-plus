ff30f9a0e31d89fbd04b1559e870e8318645da02
Daniel Aleksandersen <code@daniel.priv.no>
2012-02-09 23:23:09 +0100

    New German translation. Courtesy of chris.toph.d on Transifex.

---
+++ locales/de/includes/webm-plus-extension.i18n.js
@@ -0,0 +1,4 @@
+var oexi18n = {
+  "Click, then press Ctrl+S to save.":"Klicken, dann Strg+S zum Speichern drücken",
+  "Download Video":"Video herunterladen"
+}
---
+++ locales/de/scripts/options.i18n.js
@@ -0,0 +1,10 @@
+var oexi18n = {
+  "Add a ‘Download Video’ button below all WebM videos":"Füge einen »Video Downloaden« Button unter allen WebM-Videos hinzu",
+  "Check HTML5 Trial participation status once per session":"Überprüfe den HTML5-Test Teilnehmer-Status einmal pro Sitzung",
+  "Done":"Fertig",
+  "Embed alternate player instead of using Flash":"Alternativen Player einbinden statt Flash zu benutzen",
+  "Hide promotions for Adobe Flash Player":"Werbung für den Adobe Flash Player ausblenden",
+  "Open source on Github":"Öffne die Quell-Datei auf Github",
+  "Request WebM for embedded videos on third-party Web sites":"Bevorzuge WebM für eingebettete Videos auf Webseiten von Drittanbietern",
+  "YouTube WebM Plus options":"YouTube WebM Plus Einstellungen"
+}
